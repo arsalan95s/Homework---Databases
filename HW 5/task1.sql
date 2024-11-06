5e56404493e5942df4c1ef309021cbd688c3e29a
arsalan95s <arsalan.tahir@city.ac.uk>
2024-11-06 17:25:09 +0000

    HW 5

---
+++ HW 5/task1.sql
@@ -0,0 +1,17 @@
+--1
+SELECT bill_date, bill_total FROM restBill WHERE cust_name = 'Bob Crow';
+
+--2 
+SELECT DISTINCT cust_name FROM restBill WHERE cust_name LIKE '%Smith' ORDER BY cust_name DESC;
+
+--3
+SELECT DISTINCT cust_name FROM restBill WHERE cust_name LIKE 'C%';
+
+--4 
+SELECT first_name, surname FROM restStaff WHERE headwaiter IS NULL;
+
+--5 
+SELECT * FROM restBill WHERE (bill_date >= 160201 AND bill_date <= 160228);
+
+--6
+SELECT DISTINCT bill_date FROM restBill WHERE (bill_date >= 150101 AND bill_date <= 151231) ORDER BY bill_date;
---
+++ HW 5/task2.sql
@@ -0,0 +1,16 @@
+--1
+SELECT SUM(bill_total) AS Income FROM restBill;
+
+--2
+SELECT SUM(bill_total) AS Feb_Income FROM restBill  WHERE (bill_date >= 160201 AND bill_date <= 160228);
+
+--3
+SELECT AVG(bill_total) FROM restBill WHERE table_no = 2;
+
+--4 
+SELECT MIN(no_of_seats) AS Min, MAX(no_of_seats) AS Max, AVG(no_of_seats) AS Avg
+FROM restRest_table
+WHERE room_name = 'Blue';
+
+--5 
+SELECT COUNT(DISTINCT table_no) FROM restBill WHERE waiter_no IN (004, 002);
